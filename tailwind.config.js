5a7f3e8015111df871351953b37f5d0ef4347777
shengliang.song.ai <shengliang.song.ai@gmail.com>
2026-03-12 03:53:28 +0000

    feat(build): add tailwind v4 configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./services/**/*.{js,ts,jsx,tsx}",
-    "./constants/**/*.{js,ts,jsx,tsx}",
-    "./App.tsx",
-    "./index.tsx"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("tailwindcss-animate")
+    require('tailwindcss-animate'),
   ],
 }
